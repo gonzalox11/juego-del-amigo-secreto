38d1cc78966ccafbde00482be1da02a337fe938a
gonzalox11 <gonzaloc1918@gmail.com>
2025-01-21 14:40:41 -0300

    juego del amigo secreto

---
+++ app.js
@@ -0,0 +1,6 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+function agregarAmigo() {
+
+    let amigo = parseInt (document.getElementById('amigo').value);
+    console.log(amigo)
+}
