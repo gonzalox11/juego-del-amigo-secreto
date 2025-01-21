1da407920afe1297e39f8b6847dd74c6f6aa37ae
gonzalox11 <gonzaloc1918@gmail.com>
2025-01-21 17:23:05 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -18,6 +18,26 @@ function enumerarAmigos() {
     let amigos = document.getElementById('listaAmigos').innerHTML;
     amigos += "<li>"+nombresAmigos[nombresAmigos.length -1]+"</li>";
     document.getElementById("listaAmigos").innerHTML = amigos;
+    
+}
+
+function sorteo() {
+    let cantidadDeAmigos = nombresAmigos.length;
+    let numeroRandom = Math.floor(Math.random()*cantidadDeAmigos);
+    let amigoSorteado = nombresAmigos[numeroRandom];
+
+    console.log(amigoSorteado);
+
+    if (nombresAmigos == '') {
+        alert('por favor, ingrese nombres')
+    } else
+    {
+        let resultado = document.getElementById('resultado').innerHTML;
+        resultado = "<li>"+'el amigo es: '+amigoSorteado+"</li>";
+        document.getElementById("resultado").innerHTML = resultado;
+        document.getElementById("listaAmigos").innerHTML = "";
+        nombresAmigos = [];
+    }
 }
 
 function limpiarCaja() {
